fa014bd720401d22fa7aa9983251b2c7c0068564
Bavli Armanyous <bavli898@gmail.com>
2023-12-14 01:11:11 +0100

    working on new portfolio design

--- js/script.js
+++ js/script.js
@@ -1,27 +1,10 @@
-/* Dit zijn de elementen die zorgen voor
- de automatische animatie bij het eerste bezoek aan de pagina*/
-const image = document.querySelector(".fade-out-image");
-const fadeInElements = document.querySelector(".fadeInElements");
-const copyRightText = document.querySelector(".copyrightText");
+// button active state on current page
 
-if (!localStorage.getItem("visitedBefore")) {
-  image.addEventListener("transitionend", () => {
-    fadeInElements.classList.add("fadeIn");
-    copyRightText.classList.add("fadeIn");
-    fadeInElements.classList.remove("fadeInElements");
-  });
-
-  setTimeout(() => {
-    image.classList.add("fadeOut");
-    copyRightText.classList.add("fadeIn");
-    copyRightText.style.transition = "opacity 3.5s ease-in";
-    copyRightText.style.opacity = "1";
-  }, 1000);
-
-  localStorage.setItem("visitedBefore", true);
-} else {
-  fadeInElements.classList.add("fadeIn");
-  copyRightText.classList.add("fadeIn");
-  copyRightText.style.opacity = "1";
-  image.style.opacity = "0";
-}
+let links = document.querySelectorAll(".headerNavigation a");
+        let bodyId = document.querySelector("body").id;
+        
+        for (let link of links) {
+            if (link.dataset.active == bodyId) {
+                link.classList.add("active");
+        }
+    }
