edc565b1d6dac3a6cebecdb0126eb9203a7884f7
Bavli Armanyous <bavli898@gmail.com>
2024-01-07 20:18:01 +0100

    added dvproject2.html

--- js/switchProjects.js
+++ js/switchProjects.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const projects = [
         "../interactiveMediaProjects/imProject1.html",
+        "../interactiveMediaProjects/imProject2.html",
         "../developmentProjects/dvProject1.html",
+        "../developmentProjects/dvProject2.html",
         "../designProjects/dsProject1.html",
+        "../designProjects/dsProject2.html",
+        "../designProjects/dsProject3.html",
+        "../designProjects/dsProject4.html",
         "../researchProjects/rsProject1.html",
         "../communicationProjects/cmProject1.html",
+        "../communicationProjects/cmProject2.html",
         "../professionalIdenitityProjects/pfiProject1.html",
     ];
 
