17eb783c80447b50cdeb15b8ff90b30ec1d55b03
Bavli Armanyous <bavli898@gmail.com>
2024-01-08 10:23:44 +0100

    commented projects in switcProjects.js that hasn.t been added yet

--- js/switchProjects.js
+++ js/switchProjects.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
         "../interactiveMediaProjects/imProject2.html",
         "../developmentProjects/dvProject1.html",
         "../developmentProjects/dvProject2.html",
-        "../designProjects/dsProject1.html",
-        "../designProjects/dsProject2.html",
-        "../designProjects/dsProject3.html",
-        "../designProjects/dsProject4.html",
+        // "../designProjects/dsProject1.html",
+        // "../designProjects/dsProject2.html",
+        // "../designProjects/dsProject3.html",
+        // "../designProjects/dsProject4.html",
         "../researchProjects/rsProject1.html",
         "../communicationProjects/cmProject1.html",
-        "../communicationProjects/cmProject2.html",
+        // "../communicationProjects/cmProject2.html",
         "../professionalIdenitityProjects/pfiProject1.html",
     ];
 
