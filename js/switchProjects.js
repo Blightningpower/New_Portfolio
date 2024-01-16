9aa9583ec31c12980c1e8d9c99a780a5b674a7d5
Bavli Armanyous <bavli898@gmail.com>
2024-01-16 23:25:51 +0100

    Starting on adding rsProject3.html

--- js/switchProjects.js
+++ js/switchProjects.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "../designProjects/dsProject4.html",
         "../researchProjects/rsProject1.html",
         "../researchProjects/rsProject2.html",
+        "../researchProjects/rsProject3.html",
         "../communicationProjects/cmProject1.html",
         // "../communicationProjects/cmProject2.html",
         "../professionalIdenitityProjects/pfiProject1.html",
